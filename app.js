c1155168342188a6e8b41e5731b16f465de7a9e8
prabhat1530 <152500533+prabhat1530@users.noreply.github.com>
2025-01-17 00:19:43 +0530

    pra

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors'); // Import cors
-const userRouter = require('./routes/todo');
+const userRouter = require('./Routes/todo');
 const { mongodbConnection } = require('./connection');
 require('dotenv').config();
 
